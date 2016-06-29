06de73facd36f135a7c4945c4f7b9899007b164e
Colvine <ColvineC@dfo-mpo.ca>
2016-06-29 14:14:26 -0700

    Cleaned up project. Adjusted variable naming to conform to standard

--- BuildAndRun.bat
+++ BuildAndRun.bat
@@ -1,8 +1,4 @@
-!!! Not functional at the moment !!!
-
 ECHO Rebuild ui files and run the GUI
-pyside-uic ui_Files\MainApp.ui -o ui_MainApp.py
-python ui_Classes\MainWindow.py 
+:: pyside-uic .\ui_Files\MainApp.ui -o ui_MainApp.py
+python %cd%\ui_Classes\MainApp.py 
 PAUSE
-
-pyside-uic MainApp.ui -o ui_MainApp.py
--- utilities/TimeSeriesUtilities.py
+++ utilities/TimeSeriesUtilities.py
@@ -8,10 +8,7 @@ def formatToDateTime(year, month, day):
                                            stringFormat)
     return dateTuple
 
-
-# Should be working !!! NOT tested
 def julianToDate(julDate):
-    # print "INPUT julian date is ", julDate
     yearAmounts = 0
     curYear = 0
     daysIn = 0
@@ -24,10 +21,7 @@ def julianToDate(julDate):
         yearAmounts += 0.25
         if yearAmounts > 1:
             yearAmounts -= 1
-        # Move i to the next julian year
-        # i += (yearAmounts + 365)
     year = curYear + 2001
-    # print "daysIn are ", daysIn
     result = datetime.datetime(year, 1, 1) + datetime.timedelta(daysIn)
     month = result.month
     day = result.day
@@ -43,28 +37,6 @@ def dateToJulian(day, month, year, dayOfYear):
     # Account for leap years
     newYear += ((year - 2001) / 4)
     newYear += dayOfYear
-    print "Returning the year: ", newYear, " from ", day, month, year
-    # ----- Sandy -----
-    # newYear = int((year - 2001) * 365.25) # <- Sandy's solution
-    # ----- Howard -----
-    # if (int(year == 2004) or int(year == 2008) or int(year == 2012) or 
-    #     int(year == 2016) or int(year == 2020)):
-    #     leap = 1
-    # else: 
-    #     leap = 0
-    # if int(month) > 1:  newYear += 31
-    # if int(month) > 2:  newYear += 28 + leap
-    # if int(month) > 3:  newYear += 31
-    # if int(month) > 4:  newYear += 30
-    # if int(month) > 5:  newYear += 31
-    # if int(month) > 6:  newYear += 30
-    # if int(month) > 7:  newYear += 31
-    # if int(month) > 8:  newYear += 31
-    # if int(month) > 9:  newYear += 30
-    # if int(month) > 10:  newYear += 31
-    # if int(month) > 11:  newYear += 30
-    # newYear += int(day)    
-    # ----- Callum ----- 
     return newYear
 
 
